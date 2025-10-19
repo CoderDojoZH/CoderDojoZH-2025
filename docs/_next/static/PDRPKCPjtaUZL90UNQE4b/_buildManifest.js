af6efa0a81eca560bae915e50a28a0687523cace
bibix <bibix@>
2025-10-19 15:54:13 +0200

    Rebuilt with new base path

---
+++ docs/_next/static/PDRPKCPjtaUZL90UNQE4b/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/e67070844d7c983d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/CoderDojoZH-2025",
+        "destination": "/CoderDojoZH-2025/de"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/e986c23088d13065.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/23ecc749d2e6f366.js",
+  "static/chunks/turbopack-6a4074ed91e4d6c3.js"
+])
